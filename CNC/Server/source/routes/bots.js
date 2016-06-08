2059674dc767e75a361736b0cd143c25a5f79517
PcPriciple <fernando_gabriel@hotmail.de>
2016-06-08 19:05:31 +0200

    done?

--- CNC/Server/source/routes/bots.js
+++ CNC/Server/source/routes/bots.js
@@ -26,46 +26,42 @@ function token(res,req,next) {
 module.exports = function(router) {
 
   router.get('/Status(/:id)?',(req, res) => {
-    // falls id liefere object else array
 
     (req.params.id ? res.send(status.filter(function (value,index,array) {return (value.id == req.params.id)} )) : res.send(status));
 
   });
 
   router.post('/Status',/*token,*/(req, res) => {
+
     var ans = JSON.stringify({message:'OK'});
     var body = req.body;
     var obj;
 
-    if(body && body.id && body.status) {
-    if(body) {
+    console.log(body);
+    if(body && body.id) {
 
-    var objIndex;
-
+    // wenn obj existiert
     obj = status.filter(function (value,index,array) {
 
-      if(value.id == req.body.id) {
-        objIndex = index;
+      if(value.id == body.id) {
+
+        status[index].workload = body.status ? 1 : 0;
         return true;
       } else {
         return false;
       }
     });
 
-
-      status[objIndex].workload = req.body.status ? 1 : 0;
-
-      console.log(status[objIndex]);
-
-    } else {
+    /* if(obj.length){
       obj = {
         id: req.body.id ,
         ip: req.ip,
         task:1,
         workload: (req.body.status ? 1 : 0)
       }
-      status.push(obj);
-    }
+      status.push(JSON.stringify(obj));
+    }*/
+
   } else {
     ans = JSON.stringify({message:'NOT OK'});
   }
@@ -74,13 +70,29 @@ module.exports = function(router) {
   });
 
   router.get('/Tasks(/:id)?',(req, res) => {
-    (req.params.id ? res.send(tasks[req.params.id]): res.send(tasks))
-
+    (req.params.id ? res.send(tasks.filter(function (value,index,array) {return (value.id == req.params.id)} )) : res.send(tasks));
   });
 
-  router.post('/Tasks/:id',(req, res) => {
-    var ans = req.body.id ? JSON.stringify({message:'OK'}): JSON.stringify({message:'NOT OK'});
+  router.post('/Tasks',(req, res) => {
+    var ans = JSON.stringify({message:'NOT OK'});
+    var body = req.body;
 
-    console.log(ans);
+    if(body && body.type && body.data.input) {
+      if(body.type == 'hash-md5'|| body.type == 'hash-sha256' || body.type == 'crack-md5'){
+
+        ans = JSON.stringify({message:'OK'});
+        console.log(tasks);
+        tasks.push({
+          id: tasks.length,
+          data: {
+            input: body.data.input,
+            output: null
+          }
+        })
+        console.log(tasks);
+        fs.writeFileSync(__dirname + '/data/tasks.json',JSON.stringify(tasks), 'utf8');
+      }
+    }
+    res.send(ans);
   });
 };
